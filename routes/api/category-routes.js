f684705d797e0525c4d742ad339f4cd92100134c
ItsGeeBee <gracelbrown@hotmail.com>
2022-08-12 21:27:52 +1000

    worked on product routes. Fixed Sequelise sync issue

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,58 +3,50 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  Category.findAll(
-    {
+router.get('/', async (req, res) => {
+  try {
+    const categoryRoutes = await Category.findAll ({
     include: {
     model: Product,
       attributes: ['product_name']
     	}
-    }
-  )
-  .then(categoryData => 
-    res.json(categoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
     });
+    res.status(200).json(categoryRoutes);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 // GET one category by id
 
-router.get('/:id', (req, res) => {
-  Category.findOne({
-    where: {
-      id: req.params.id
-    },
+router.get('/:id', async (req, res) => {
+ try{
+  const categoryRoutes = await Category.findOne(req.params.id, {
     include: {
       model: Product,
       attributes: ['category_id']
     }
-  })
-    .then(categoryData => 
-      res.json(categoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  });
+  res.status(200).json(categoryRoutes);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 // POST a new category
-router.post('/', (req, res) => {
-  Category.create({
+router.post('/', async (req, res) => {
+  try{
+    const categoryRoutes = await Category.create({
     category_name: req.body.category_name
-  })
-    .then(categoryData => 
-      res.json(categoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  });
+res.status(200).json(categoryRoutes);
+} catch (err) {
+res.status(400).json(err);
+}
 });
 
 
-router.put('/:id', (req, res) => {
-  Category.update(
-    {
+router.put('/:id', async (req, res) => {
+  try{
+  const categoryRoutes = await Category.update({
       category_name: req.body.category_name
     },
     {
@@ -62,17 +54,10 @@ router.put('/:id', (req, res) => {
         id: req.params.id
       }
     })
-    .then(categoryData => {
-      if (!categoryData) {
-        res.status(404).json({ message: 'No Category found with that ID.' });
-        return;
-      }
-      res.json(categoryData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.status(200).json(categoryRoutes);
+} catch (err) {
+  res.status(400).json({ message: 'No category found with that id!' });
+}
 });
 
 // DELETE a category by id
